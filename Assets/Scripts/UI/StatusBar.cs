02e852e4359026bbe7cd61475bbef0337745960a
JoMoJg <jordmonf@gmail.com>
2022-11-19 00:10:57 -0400

    .

--- Assets/Scripts/UI/StatusBar.cs
+++ Assets/Scripts/UI/StatusBar.cs
@@ -6,15 +6,19 @@ public class StatusBar : MonoBehaviour
 {
     [SerializeField] Transform bar;
     private float currentHP;
+    private float currentArmor;
 
     public void Update()
     {
         bar.transform.localScale = new Vector3(currentHP, 0.05f,1f);
         Debug.Log(currentHP);
+
+        bar.transform.localScale = new Vector3(currentArmor, 0.05f, 1f);
     }
 
     public void SetState(float current, float max) 
     {
         this.currentHP = current/max;
+        this.currentArmor = current / max;
     }
 }
